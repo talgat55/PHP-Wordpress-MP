6f816659a71c4957833486b71fd1b32f4a14dc13
talgat55 <lightxdesign55@gmail.com>
2020-06-18 17:10:20 +0600

    fix add link in news item in image and title

--- wp-content/themes/asmart/inc/news-item.php
+++ wp-content/themes/asmart/inc/news-item.php
@@ -9,13 +9,17 @@
 <li class="news-articles__item col-12 col-lg-3">
     <div class="news-articles__item_wrapper">
         <div class="news-articles__image">
-            <img
-                src="<?= wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), "home-news-articles")[0]; ?>"
-            />
+            <a href="<?php echo get_the_permalink(get_the_ID()); ?>">
+                <img
+                        src="<?= wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), "home-news-articles")[0]; ?>"
+                />
+            </a>
         </div>
         <div class="news-articles__item-content">
             <h3 class="news-articles__title">
-                <?php echo get_the_title(get_the_ID()); ?>
+                <a href="<?php echo get_the_permalink(get_the_ID()); ?>">
+                    <?php echo get_the_title(get_the_ID()); ?>
+                </a>
             </h3>
             <div class="news-articles____text">
                 <?php echo mb_strimwidth(strip_tags(get_the_content(get_the_ID())), 0, 150, "..."); ?>
